595cde02253d6eb2d41a97b1043f1c9919c11da0
Dhyey Nikalwala <61624477+Dhyey-Nikalwala@users.noreply.github.com>
2021-01-30 14:58:08 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+# from urllib import quote      #Uncomment line below to use python 2
+from urllib.parse import quote  #Uncomment line below to use python 3 
+
+from time import sleep
+# %%from pyvirtualdisplay import Display
+# display = Display(visible=0, size=(800, 600))
+# display.start()
+
+#update css selector if you have any issues
+
+css_selector = "#main > footer > div._3SvgF._1mHgA.copyable-area > div.DuUXI > div > div._1awRl.copyable-text.selectable-text"
+
+# message to be sent to everyone, you can also read it as a dict from a file with ph nos as keys
+msg = '''
+You can try all sorts of formatting like _italics_ and *Kindly Click on The link to join the Group* or _*This Message is for testing purpose only*_. 
+
+Multiline works too.
+
+And so do links like https://chat.whatsapp.com/KSjYqr0xywsBJtfCtcJfIs
+'''     
+
+driver = webdriver.Chrome()
+
+phone = []                                                      #enter comma separated 10 digit phone numbers here or read them from the numbers_file
+with open ('numbers.txt') as numbers_file:                    #uncomment these three three lines to read input from numbers.txt file
+    for line in numbers_file:
+    	line=line.strip()
+    	if len (line)==10:								   		#skip numbers of length not equal to 10
+    		phone.append(str(line))
+# phone.extend(str(raw_input("Enter the comma separated list of numbers (Press enter to skip)\n")).split(','))
+
+msg = quote(msg)  # url-encode the message, use other functios for handling dictionaries, not recommended
+driver.get("https://web.whatsapp.com")  # first call without delay in order to scan qr code
+sleep(2)
+failed_list = []
+for index, number in enumerate(phone, 1):
+    url = "https://web.whatsapp.com/send?phone=91" + number + "&text=" + msg
+    driver.get(url)
+    TRIES = 20
+
+    sleep(2)  # any delay is okay, even 0, but 3-5 seems appropriate
+    for i in range(TRIES):
+        try:
+            driver.find_element_by_css_selector(css_selector).send_keys(Keys.RETURN)
+            sleep(5)
+            driver.execute_script("window.onbeforeunload = function() {};")
+            sleep(5)
+            print (f'Sent to {index} : {number}')
+            break
+        except:
+            print("not yet")
+            sleep(1)
+        
+    else:
+        failed_list.append(number)
+    
+print ("Done")
+
+if (len(failed_list)==0):
+    print (f'Message successfully sent to all {len(phone)} numbers.')
+else:
+    print (f'Message sent to all numbers EXCEPT:')
+    for number in failed_list:
+        print (number)
+    
+driver.quit()                                                 #uncomment to close chrome window as scoon as program ends
